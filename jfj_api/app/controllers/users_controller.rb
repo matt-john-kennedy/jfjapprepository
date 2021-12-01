b12df6e3ed4ede9a1f40f1c85a010a5ac6d2b57c
Adriana Heesh <adrianaheesh@gmail.com>
2021-12-01 21:13:18 +1000

    created rails api, user controller and model

---
+++ jfj_api/app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.create(user_params)
+        if @user.save 
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {email: @user.email, role: @user.role, jwt: auth_token.token}, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity
+        end
+    end
+    
+    def sign_in
+        @user = User.find_by_email(params[:email])
+        if @user && @user.authenticate(params[:password])
+            auth_token = Knock::AuthToken.new payload: {sub: @user.id}
+            render json: {email: @user.email, role: @user.role, jwt: auth_token.token}, status: 200
+        else
+            render json: {error: "Incorrect username or password"}, status: 404
+        end
+    end
+
+    def destroy
+        
+    end
+
+    private
+    def user_params
+        params.permit(:email, :password, :password_confirmation, :role)
+    end
+
+end
