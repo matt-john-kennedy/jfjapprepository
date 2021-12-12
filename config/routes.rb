219c84bc98bc42816f4cde1f704f07c92acf2540
Adriana Heesh <adrianaheesh@gmail.com>
2021-12-13 09:39:26 +1000

    changed routes - removed autogenerated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookings/index'
-  get 'bookings/create'
-  get 'bookings/show'
-  get 'bookings/update'
-  get 'bookings/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/api' do
     get '/castles', to: 'castles#index'
@@ -20,18 +15,18 @@ Rails.application.routes.draw do
     delete '/customers/:id', to: 'customers#destroy'
     
     # routes for all enquiries
-    get 'enquiries', to: 'enquiries#index'
-    post 'enquiries', to: 'enquiries#create'
-    get 'enquiries/:id', to: 'enquiries#show'
-    put 'enquiries/:id', to: 'enquiries#update'
-    delete 'enquiries/:id', to: 'enquiries#destroy'
+    get '/enquiries', to: 'enquiries#index'
+    post '/enquiries', to: 'enquiries#create'
+    get '/enquiries/:id', to: 'enquiries#show'
+    put '/enquiries/:id', to: 'enquiries#update'
+    delete '/enquiries/:id', to: 'enquiries#destroy'
 
     # routes for all bookings
-    get 'bookings', to: 'bookings#index'
-    post 'bookings', to: 'bookings#create'
-    get 'bookings/:id', to: 'bookings#show'
-    put 'bookings/:id', to: 'bookings#update'
-    delete 'bookings/:id', to: 'bookings#destroy'
+    get '/bookings', to: 'bookings#index'
+    post '/bookings', to: 'bookings#create'
+    get '/bookings/:id', to: 'bookings#show'
+    put '/bookings/:id', to: 'bookings#update'
+    delete '/bookings/:id', to: 'bookings#destroy'
 
       scope '/auth' do
         post '/sign_up', to: 'users#create'
