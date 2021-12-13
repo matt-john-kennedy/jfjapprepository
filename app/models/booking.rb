fd7a3e9c0d4687b2be80daed7bf0fe0b6ea81528
Adriana Heesh <adrianaheesh@gmail.com>
2021-12-13 13:48:20 +1000

    added validates to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :castle
   belongs_to :customer
   belongs_to :enquiry
+
+  validates :start_time, :end_time, :duration, :total, :paid, :terms_agreement, presence: true
 end
--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -2,4 +2,10 @@ class Enquiry < ApplicationRecord
   belongs_to :castle
   belongs_to :customer
   has_many :bookings
+
+  validates :terms_agreement, :duration, :end_time, :start_time, :paid, presence: true
+
+  # do I have to put this when a relationship is outlined above? 
+  # validates :castle_id, presence: true
+  # validates :customer_id, presnece: true
 end
