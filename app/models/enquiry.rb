703a5a0dae35407a5b5112b1ae2773ac3da396cc
Adriana Heesh <adrianaheesh@gmail.com>
2021-12-13 14:37:28 +1000

    updated validation of postcode so length is 4

--- app/models/enquiry.rb
+++ app/models/enquiry.rb
@@ -4,8 +4,4 @@ class Enquiry < ApplicationRecord
   has_many :bookings
 
   validates :terms_agreement, :duration, :end_time, :start_time, :paid, presence: true
-
-  # do I have to put this when a relationship is outlined above? 
-  # validates :castle_id, presence: true
-  # validates :customer_id, presnece: true
 end
