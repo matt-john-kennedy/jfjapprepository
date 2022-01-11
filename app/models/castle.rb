fd1fbff8a456624de551769d6e4fb47c87afb0bf
Adriana Heesh <adrianaheesh@gmail.com>
2022-01-11 13:33:28 +1000

    permitted deployed front end link to cors.rb

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,7 +1,6 @@
 class Castle < ApplicationRecord
     has_many :enquiries, dependent: :destroy
     has_many :bookings, dependent: :destroy
-    has_many :image_urls
 
     validates :name, uniqueness: true, presence: true
     validates :description, presence: true
